bd5118e7f129cfba8434fe7deaef5a3e7ae9db8a
Piotr Wolny <piotr.wolny@alan-systems.com>
2024-12-05 11:03:23 +0100

    2024 - day 05 - init part 2

---
+++ 2024/day05/part2/int_parse.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func parseInt(s string) int {
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	return num
+}
