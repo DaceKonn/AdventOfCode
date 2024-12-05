10aec0c4726d84ef90579bdfecfcd6002094ffe1
Piotr Wolny <piotr.wolny@alan-systems.com>
2024-12-05 13:52:09 +0100

    2024 - day 05 - part 2 complete

--- 2024/day05/part2/main.go
+++ 2024/day05/part2/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
-const file string = "input_test.txt"
+const file string = "input_real.txt"
 
 func main() {
 	fmt.Println("Advent of Code 2024 - day 5")
@@ -23,58 +24,125 @@ func main() {
 
 	headerLog(1, "rules result")
 	log(rules)
+	orderRules(rules)
+	log(rules)
+
+	headerLog(0, "Get super rules")
+	superRules := getSuperRules(rules)
+
+	headerLog(1, "super rules result")
+	log(superRules)
 
 	headerLog(1, "updates result")
 	logUpdates(updates)
 
 	headerLog(0, "Evaluate updates")
-	evaluateUpdates(updates, rules)
+	evaluateUpdates(updates, superRules)
 
 	headerLog(1, "updates evaluation result")
-	logUpdates(updates)
+	// logUpdates(updates)
 
-	headerLog(0, "Sum middle page numbers")
-	sum := sumValidMiddlePages(updates)
-
-	headerLog(1, "Result")
-	log(sum)
-}
-
-func sumValidMiddlePages(updates []update) int {
-	var result int = 0
-	for i, update := range updates {
-		if !update.valid {
-			depthLog(1, fmt.Sprint("update: ", i, "\t\tskip"))
+	for i, up := range updates {
+		if up.recursionDepth == 0 {
 			continue
 		}
-		depthLog(1, fmt.Sprint("update: ", i, "\t\tsum", "\t\t", update.getMiddlePageNumber()))
-		result += update.getMiddlePageNumber()
+		log(i, " : ", up.initPages, " : ", up.recursionDepth)
+	}
+
+	headerLog(0, "Sum middle page numbers")
+	validSum, fixedSum, unfixed := sumMiddlePages(updates)
+
+	headerLog(1, "unfixed")
+	log(unfixed)
+	headerLog(1, "Valid Result")
+	log(validSum)
+	headerLog(1, "Fixed Result")
+	log(fixedSum)
+}
+
+func getSuperRules(rules []rule) map[int][]rule {
+	var result map[int][]rule = make(map[int][]rule)
+	for _, rule := range rules {
+		result[rule.x] = append(result[rule.x], rule)
 	}
 	return result
 }
 
-func evaluateUpdates(updates []update, rules []rule) {
+func sumMiddlePages(updates []update) (validSum, fixedSum, unfixed int) {
+	for i, update := range updates {
+		if !update.valid && !update.fixed {
+			depthLog(1, fmt.Sprint("update: ", i, "\t\tunfixed"))
+			unfixed++
+			continue
+		}
+		if !update.valid && update.fixed {
+			depthLog(1, fmt.Sprint("update: ", i, "\t\tfixed", "\t\t", update.getMiddlePageNumber()))
+			fixedSum += update.getMiddlePageNumber()
+			continue
+		}
+		depthLog(1, fmt.Sprint("update: ", i, "\t\tvalid", "\t\t", update.getMiddlePageNumber()))
+		validSum += update.getMiddlePageNumber()
+	}
+	return validSum, fixedSum, unfixed
+}
+
+func evaluateUpdates(updates []update, superRules map[int][]rule) {
 	for updateIndx, update := range updates {
 		depthLog(1, fmt.Sprint("evaluating update: ", updateIndx))
 		depthLog(2, fmt.Sprint(update.pages))
+		evaluateUpdateRecursive(updates, updateIndx, superRules, 3, 0)
+		depthLog(2, fmt.Sprint(update.pages))
+		fmt.Print("\n\n")
+	}
+}
+
+func evaluateUpdateRecursive(updates []update, updateIndex int, superRules map[int][]rule, logDepth int, recursionDepth int) {
+	depthLog(3, fmt.Sprint("recursion depth: ", recursionDepth, " update: ", updateIndex))
+	updates[updateIndex].recursionDepth = recursionDepth
+
+	if recursionDepth > 1 {
+		depthLog(logDepth, "recursion reached limit")
+		return
+	}
+
+	for x, rules := range superRules {
+		// depthLog(logDepth, fmt.Sprint("processing rule: ", rule.x, "|", rule.y))
+		xValue, xExists := updates[updateIndex].indexedPages[x]
+		if !xExists {
+			continue
+		}
+		var applicableValues []int
 		for _, rule := range rules {
-			depthLog(3, fmt.Sprint("processing rule: ", rule.x, "|", rule.y))
-			xValue, xExists := update.indexedPages[rule.x]
-			yValue, yExists := update.indexedPages[rule.y]
-
-			depthLog(3, fmt.Sprint("x:", xExists, "|y:", yExists))
-
-			if !xExists || !yExists {
+			yValue, yExists := updates[updateIndex].indexedPages[rule.y]
+			if !yExists {
 				continue
 			}
-
-			depthLog(3, fmt.Sprint("x:", xValue, "|y:", yValue))
-
-			if xValue >= yValue {
-				depthLog(4, fmt.Sprint("Update: ", updateIndx, " violation of rule: ", rule.x, "|", rule.y))
-				updates[updateIndx].valid = false
-			}
+			applicableValues = append(applicableValues, yValue)
+			depthLog(logDepth+1, fmt.Sprint("Update: ", updateIndex, " violation of rule: ", rule.x, "|", rule.y))
 		}
+		// depthLog(logDepth, fmt.Sprint("x:", xExists, "|y:", yExists))
+
+		if len(applicableValues) == 0 {
+			continue
+		}
+
+		// depthLog(logDepth, fmt.Sprint("x:", xValue, "|y:", yValue))
+		sort.Ints(applicableValues)
+		yValue := applicableValues[0]
+
+		if xValue >= yValue {
+			depthLog(logDepth+2, fmt.Sprint("x:", xValue, "|y:", yValue))
+			updates[updateIndex].valid = false
+			updates[updateIndex].fixed = false
+			updates[updateIndex].fixPageIndex(x, yValue)
+			updates[updateIndex].refreshPages()
+			continue
+		}
+		updates[updateIndex].fixed = true
+	}
+
+	if !updates[updateIndex].valid && !updates[updateIndex].fixed {
+		evaluateUpdateRecursive(updates, updateIndex, superRules, logDepth+1, recursionDepth+1)
 	}
 }
 
@@ -87,19 +155,20 @@ func parseLines(rawLines []string) (rules []rule, updates []update) {
 		}
 		switch rulesSection {
 		case true:
-			depthLog(1, "processing rule line")
+			// depthLog(1, "processing rule line")
 			split := strings.Split(line, "|")
 			x := parseInt(split[0])
 			y := parseInt(split[1])
 			rules = append(rules, rule{x, y})
 		case false:
-			depthLog(1, "processing update line")
+			// depthLog(1, "processing update line")
 			update := newEmptyUpdate()
 			split := strings.Split(line, ",")
 			for i, strNum := range split {
 				num := parseInt(strNum)
 				update.indexedPages[num] = i
 				update.pages = append(update.pages, num)
+				update.initPages = append(update.initPages, num)
 			}
 			updates = append(updates, update)
 		}
@@ -110,9 +179,10 @@ func parseLines(rawLines []string) (rules []rule, updates []update) {
 
 func logUpdates(updates []update) {
 	for i, u := range updates {
-		depthLog(1, fmt.Sprint("Update: ", i))
-		depthLog(2, fmt.Sprint("valid:", u.valid))
-		depthLog(2, fmt.Sprint("pages: ", u.pages))
-		depthLog(2, fmt.Sprint("indexed: ", u.indexedPages))
+		depthLog(1, fmt.Sprint("Update:\t", i))
+		depthLog(2, fmt.Sprint("valid:\t", u.valid))
+		depthLog(2, fmt.Sprint("fixed:\t", u.fixed))
+		depthLog(2, fmt.Sprint("pages:\t", u.pages))
+		depthLog(2, fmt.Sprint("indexed:\t", u.indexedPages))
 	}
 }
--- 2024/day05/part2/update.go
+++ 2024/day05/part2/update.go
@@ -7,21 +7,62 @@ type update struct {
 	//pages in update represented as a map of page and it's place(index) in update -> [pageNr]index
 	indexedPages map[int]int
 	//raw page numbers
-	pages []int
+	pages     []int
+	initPages []int
+	//if invalid update was fixed
+	fixed          bool
+	recursionDepth int
 }
 
 func newEmptyUpdate() update {
 	return update{
-		valid:        true,
-		indexedPages: make(map[int]int),
-		pages:        make([]int, 0),
+		valid:          true,
+		fixed:          false,
+		indexedPages:   make(map[int]int),
+		pages:          make([]int, 0),
+		initPages:      make([]int, 0),
+		recursionDepth: 0,
 	}
 }
 
 func (u update) getMiddlePageNumber() int {
 	if len(u.pages)%2 == 0 {
+		log(u.pages)
 		panic("what is the middle?!")
 	}
 	middle := (len(u.pages) / 2)
 	return u.pages[middle]
+}
+
+func (u *update) fixPageIndex(pageNr int, newPlace int) {
+	switch newPlace >= 0 {
+	case true:
+		oldPlace := u.indexedPages[pageNr]
+		u.indexedPages[pageNr] = newPlace
+		for page, index := range u.indexedPages {
+			if page == pageNr {
+				continue
+			}
+			if index >= newPlace && index < oldPlace {
+				u.indexedPages[page] = index + 1
+			}
+			// } else if index > oldPlace && index < newPlace {
+			// 	u.indexedPages[page] = index - 1
+			// }
+		}
+	case false:
+		// u.indexedPages[pageNr] = 0
+		// for page, index := range u.indexedPages {
+		// 	if page == pageNr {
+		// 		continue
+		// 	}
+		// 	u.indexedPages[page] = index + 1
+		// }
+	}
+}
+
+func (u *update) refreshPages() {
+	for page, index := range u.indexedPages {
+		u.pages[index] = page
+	}
 }
